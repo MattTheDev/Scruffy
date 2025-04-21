06749db8c96de1c4657b9083fddd453308a14eda
Matt Smith <v-smithmatt@microsoft.com>
2025-04-21 17:27:18 -0500

    Tesitng User Commands ...

---
+++ src/Scruffy/Commands/User/Points.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Interactions;
+
+namespace Scruffy.Commands.User;
+
+public class Points : InteractionModuleBase
+{
+    [UserCommand("grant")]
+    public async Task GrantAsync()
+    {
+        await DeferAsync();
+
+        await FollowupAsync("Context Menu Command Clicked");
+    }
+}
