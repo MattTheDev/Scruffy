4dceb3a7ffd60c69cb84529b230445a407299a66
Matt Smith <v-smithmatt@microsoft.com>
2025-04-20 21:26:42 -0500

    Move message creation to Bot

--- src/Scruffy/Commands/Slash/Role.cs
+++ src/Scruffy/Commands/Slash/Role.cs
@@ -10,31 +10,23 @@ public class Role : InteractionModuleBase
         "Command to configure reaction, role, and message for access.",
         true,
         RunMode.Async)]
-    public async Task SetupAsync(string messageId,
+    public async Task SetupAsync(string body, 
+        string messageId,
         string emote,
         string label,
         IRole role)
     {
         await DeferAsync(true);
 
-        var validMessage = await Context.Channel.GetMessageAsync(ulong.Parse(messageId)).ConfigureAwait(false);
-
-        if (validMessage == null)
-        {
-            await FollowupAsync("Please provide a valid message ID.");
-            return;
-        }
+        var embedBuilder = new EmbedBuilder();
+        embedBuilder.Description = body;
 
         var componentBuilder = new ComponentBuilder()
             .WithButton(label,
                 $"rr:{role.Id}",
-                emote: new Emoji(emote));
+                emote: new Emoji(emote)); ;
 
-        await Context.Channel.ModifyMessageAsync(validMessage.Id, (properties) =>
-        {
-            properties.Components = new Optional<MessageComponent>(componentBuilder.Build());
-        });
-
-        await FollowupAsync("Role button has been successfully added.");
+        await FollowupAsync(embed: embedBuilder.Build(),
+            components: componentBuilder.Build());
     }
 }
